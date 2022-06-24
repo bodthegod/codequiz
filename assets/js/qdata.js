f813ad694f756fdbc2f16aead900ce1298b31b01
bodthegod <bodthegod@gmail.com>
2022-06-24 12:09:58 +0000

    Add question data file in js and define first question

---
+++ assets/js/qdata.js
@@ -0,0 +1,15 @@
+// question data for js
+
+let qdata = [
+    {
+        qnumber: 1,
+        qname: "What is Javascript Used for?",
+        qanswer: "To create dynamic and interactive web content",
+        qselection: [
+            "To create dynamic and interactive web content",
+            "To style websites and change the appearance",
+            "To create basic websites",
+            "To view databases for websites",
+        ]
+    }
+]
