12d3b17c5770773545171bb4496ed542edbfa72d
bodthegod <bodthegod@gmail.com>
2022-06-24 11:30:10 +0000

    Add onclick function to use showBox class in CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,8 @@ const btn_start = document.querySelector(".btn-start button");
 const guide_box = document.querySelector(".guide-box");
 const btn_exit = guide_box.querySelector(".buttons .exit");
 const btn_continue = guide_box.querySelector(".buttons .restart");
+
+btn_start.onclick = ()=>{
+    guide_box.classList.add("showBox");
+
+}
