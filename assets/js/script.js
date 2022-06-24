86916a6d929c80b0ef76828ded6350cff2af0fa0
bodthegod <bodthegod@gmail.com>
2022-06-24 13:18:56 +0000

    Add displayQuestion function to display questions within the game-area

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,12 @@ const game_area = document.querySelector(".game-area");
 const btn_exit = guide_box.querySelector(".buttons .exit");
 const btn_continue = document.querySelector(".buttons .restart");
 
-
 // start button onclick function
-
 btn_start.onclick = ()=>{
     guide_box.classList.add("showBox");
 }
 
 // exit button onclick function
-
 btn_exit.onclick = ()=>{
     guide_box.classList.remove("showBox");
 }
@@ -22,4 +19,20 @@ btn_exit.onclick = ()=>{
 btn_continue.onclick = ()=>{
     guide_box.classList.remove("showBox");
     game_area.classList.add("showGame");
+// calls displayQuestion function
+    displayQuestion(3);
+}
+
+let question_counter = 0;
+
+//function to display questions
+function displayQuestion(index){
+    const question_display = document.querySelector("#question-display");
+    const answer_select = document.querySelector("#answer-select");
+    let question_text = '<span>'+ qdata[index].qname + '</span>';
+    let answer_text = '<div class="answer"><span></span></div>'
+                    + '<div class="answer"><span></span></div>'
+                    + '<div class="answer"><span></span></div>'
+                    + '<div class="answer"><span></span></div>'
+    question_display.innerHTML = question_text;
 }
