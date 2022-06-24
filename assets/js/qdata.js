ad4a8ba267eac964f9333435c9159bc431284c8e
bodthegod <bodthegod@gmail.com>
2022-06-24 13:01:24 +0000

    Add all questions in qdata file

--- assets/js/qdata.js
+++ assets/js/qdata.js
@@ -11,5 +11,104 @@ let qdata = [
             "To create basic websites",
             "To view databases for websites",
         ]
-    }
-]
+    },
+    {
+        qnumber: 2,
+        qname: "In JavaScript, what element is used to store and manipulate text, usually in multiples?",
+        qanswer: "Strings",
+        qselection: [
+            "Variables",
+            "Strings",
+            "Arrays",
+            "Recorders",
+        ]
+    },
+    {
+        qnumber: 3,
+        qname: "What is the element called that is used to describe the set of variables, objects, and functions you explicitly have access to?",
+        qanswer: "Scope",
+        qselection: [
+            "Scope",
+            "Rescriction",
+            "Range",
+            "Output Level",
+        ]
+    },
+    {
+        qnumber: 4,
+        qname: "How do you create a transition effect in CSS?",
+        qanswer: "transition: all 0.5s ease;",
+        qselection: [
+            "transition: duration: 2s;",
+            "translate-transition: 10s;",
+            "scale-object: 2,4;",
+            "transition: all 0.5s ease;",
+        ]
+    },
+    {
+        qnumber: 5,
+        qname: "How do you create rounded corners in CSS?",
+        qanswer: "border-radius: 15px;",
+        qselection: [
+            "border-radius: 15px",
+            "corner-round;",
+            "object-rounded: yes;",
+            "None",
+        ]
+    },
+    {
+        qnumber: 6,
+        qname: "Which of these elements are all <table> elements?",
+        qanswer: "<table><tr><td>",
+        qselection: [
+            "<table><body>",
+            "<tt><tablefooter>",
+            "<table><tr><td>",
+            "<head><tr>",
+        ]
+    },
+    {
+        qnumber: 7,
+        qname: "Which of these are NOT a programming language?",
+        qanswer: "Spacer",
+        qselection: [
+            "TypeScript",
+            "JavaScript",
+            "Spacer",
+            "Python",
+        ]
+    },
+    {
+        qnumber: 8,
+        qname: "Python is a ______ programming language",
+        qanswer: "High-level",
+        qselection: [
+            "High-level",
+            "Mid-level",
+            "Low-level",
+            "None of the above",
+        ]
+    },
+    {
+        qnumber: 9,
+        qname: "How many heading tags are in HTML?",
+        qanswer: "6",
+        qselection: [
+            "8",
+            "6",
+            "4",
+            "2",
+        ]
+    },
+    {
+        qnumber: 10,
+        qname: "The SCRIPT tag allows you to link to which programming language?",
+        qanswer: "Javascript",
+        qselection: [
+            "JavaScript",
+            "SQL",
+            "Pascal",
+            "Python",
+        ]
+    },
+];
