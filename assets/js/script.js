20ffa1e2e75319c112c9ce8b315f301c1c42b119
bodthegod <bodthegod@gmail.com>
2022-06-24 12:01:47 +0000

    Add functions to all buttons in js to add and remove boxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,25 @@
 // setting up all objects
 const btn_start = document.querySelector(".btn-start button");
 const guide_box = document.querySelector(".guide-box");
+const game_area = document.querySelector(".game-area");
 const btn_exit = guide_box.querySelector(".buttons .exit");
-const btn_continue = guide_box.querySelector(".buttons .restart");
+const btn_continue = document.querySelector(".buttons .restart");
+
+
+// start button onclick function
 
 btn_start.onclick = ()=>{
     guide_box.classList.add("showBox");
+}
 
+// exit button onclick function
+
+btn_exit.onclick = ()=>{
+    guide_box.classList.remove("showBox");
+}
+
+// continue button onclick function
+btn_continue.onclick = ()=>{
+    guide_box.classList.remove("showBox");
+    game_area.classList.add("showGame");
 }
