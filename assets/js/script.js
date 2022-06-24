b583dcf2d588b260e2a2107080b1dc18a3bf65be
bodthegod <bodthegod@gmail.com>
2022-06-24 11:23:27 +0000

    Define buttons within JS to apply uses to quiz

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// setting up all objects
+const btn_start = document.querySelector(".btn-start button");
+const guide_box = document.querySelector(".guide-box");
+const btn_exit = guide_box.querySelector(".buttons .exit");
+const btn_continue = guide_box.querySelector(".buttons .restart");
